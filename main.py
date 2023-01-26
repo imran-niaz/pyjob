86f2db425b864a6f71a277931ffa586a12521add
imran <info.iosnapk@gmail.com>
2023-01-26 05:27:03 -0500

    upd

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import datetime
+from fpdf import FPDF
 
 class JobApplication:
     def __init__(self, first_name, last_name, dob, address, prev_job_exp):
@@ -25,8 +25,22 @@ class JobApplication:
             return False
         return True
 
+    def save_to_pdf(self):
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font("Arial", size=12)
+        pdf.cell(200, 10, txt="Job Application", ln=1, align="C")
+        pdf.cell(200, 10, txt="Name: {} {}".format(self.first_name, self.last_name), ln=1)
+        pdf.cell(200, 10, txt="Date of Birth: {}".format(self.dob), ln=1)
+        pdf.cell(200, 10, txt="Address: {}".format(self.address), ln=1)
+        pdf.cell(200, 10, txt="Previous Job Experience:", ln=1)
+        for exp in self.prev_job_exp:
+            pdf.cell(200, 10, txt="- {}".format(exp), ln=1)
+        pdf.output("job_application.pdf")
+
     def submit_application(self):
         if self.validate_dob() and self.validate_address() and self.validate_prev_job_exp():
+            self.save_to_pdf()
             print("Application submitted successfully.")
         else:
             print("Application submission failed. Please check your information and try again.")
